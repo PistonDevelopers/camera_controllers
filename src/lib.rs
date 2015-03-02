aee5ed2e5bfc1cfc10e54ce3da2642eb4631181a
stjahns <s.t.jahns@gmail.com>
2015-03-01 20:37:50 -0700

    Added 'first_person' and 'orbit_zoom_camera'

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,32 @@
-#[test]
-fn it_works() {
-}
+#![crate_name = "camera_controllers"]
+#![deny(missing_docs)]
+#![feature(core, std_misc)]
+
+//! A library for 3D camera control
+
+#[macro_use]
+extern crate bitflags;
+extern crate event;
+extern crate input;
+extern crate vecmath;
+extern crate quaternion;
+extern crate cam;
+
+pub use cam::{
+    Camera,
+    CameraPerspective,
+    model_view_projection,
+};
+
+pub use first_person::{
+    FirstPerson,
+    FirstPersonSettings,
+};
+
+pub use orbit_zoom_camera::{
+    OrbitZoomCamera,
+    OrbitZoomCameraSettings,
+};
+
+mod first_person;
+mod orbit_zoom_camera;
